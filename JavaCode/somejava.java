2ef3267e5d7b8d248b20a4cd036e532c9e9429cb
Siddharth Singh <siddharth12375@gmail.com>
2021-06-19 20:12:17 +0530

    some changes

--- JavaCode/somejava.java
+++ JavaCode/somejava.java
@@ -1,1 +1,16 @@
 //some java code
+package learnJava;
+
+public class somejava {
+	public static void main(String[] args) {
+		helloWorld();
+	}
+
+	public void helloWord() {
+		System.out.println("Hello, World!");
+	}
+
+	public void greeting(String name) {
+		System.out.println("Hey, How are you doing?." + name);
+	}
+} 
